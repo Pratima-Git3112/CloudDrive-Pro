fa5d2340d41a56e4e88f4ec89a1641f6beb3a43e
Pratima Dudhade <pratimadudhde800@gmail.com>
2026-07-28 10:36:52 +0530

    Configured SQLAlchemy and created User model

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 
+from app.config.settings import APP_NAME, APP_VERSION
+from app.database.database import Base, engine
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
-    title="CloudDrive Pro API",
-    description="Secure Cloud File Storage Platform",
-    version="1.0.0"
+    title=APP_NAME,
+    version=APP_VERSION,
+    description="Secure Cloud File Storage Platform"
 )
+
 
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to CloudDrive Pro",
+        "message": f"Welcome to {APP_NAME}",
         "status": "Running Successfully"
     }
